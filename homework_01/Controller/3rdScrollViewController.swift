494e4f058278f3412b7e434ec80340a22d3f0319
Nicole <goolitesum@gmail.com>
2021-06-30 18:18:29 +0800

    3rd finish

--- homework_01/Controller/3rdScrollViewController.swift
+++ homework_01/Controller/3rdScrollViewController.swift
@@ -10,13 +10,31 @@ import UIKit
 class _rdScrollViewController: UIViewController {
     var location : [ShowData] = []
     
+    @IBOutlet weak var imageInThirdView: UIImageView!
+    @IBOutlet weak var title1InThirdView: UILabel!
+    @IBOutlet weak var title2InThirdView: UILabel!
+    @IBOutlet weak var contentInThirdView: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //let i = Int.random(in: 0...location.count-1)
+        imageInThirdView.load(urlString: location[2].infoImage)
+        title1InThirdView.text = location[2].title1Text
+        title2InThirdView.text = location[2].title2Text
+        contentInThirdView.text = location[2].contentText
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func clickChange(_ sender: UIButton) {
+        let i = Int.random(in: 0...location.count-1)
+        imageInThirdView.load(urlString: location[i].infoImage)
+        title1InThirdView.text = location[i].title1Text
+        title2InThirdView.text = location[i].title2Text
+        contentInThirdView.text = location[i].contentText
+    }
+    
     /*
     // MARK: - Navigation
 
